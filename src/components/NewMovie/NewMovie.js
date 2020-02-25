d70ede9cd7530a020b9ac500e6e5fd0a4956b930
Yurii Holiuk <bionicuss@gmail.com>
2020-02-25 14:51:48 +0200

    lesson code

--- src/components/NewMovie/NewMovie.js
+++ src/components/NewMovie/NewMovie.js
@@ -1,13 +1,121 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { TextField } from '../TextField';
 
 export class NewMovie extends Component {
-  state = {};
+  state = {
+    title: '',
+    description: '',
+    imgUrl: '',
+    imdbUrl: '',
+    imdbId: '',
+    titleError: '',
+  };
+
+  handleChange = (event) => {
+    const { target: { name, value } } = event;
+
+    this.setState({ [name]: value });
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    const {
+      title,
+      description,
+      imgUrl,
+      imdbUrl,
+      imdbId,
+    } = this.state;
+
+    if (title === '') {
+      this.setState({
+        titleError: 'Title is required',
+      });
+
+      return;
+    }
+
+    this.props.onAdd({
+      title,
+      description,
+      imgUrl,
+      imdbUrl,
+      imdbId,
+    });
+
+    this.setState({
+      title: '',
+      description: '',
+      imgUrl: '',
+      imdbUrl: '',
+      imdbId: '',
+      titleError: '',
+    });
+  }
 
   render() {
+    const {
+      title,
+      description,
+      imgUrl,
+      imdbUrl,
+      imdbId,
+      titleError,
+    } = this.state;
+
     return (
-      <form>
-        Put the form here
+      <form name="newMovie" onSubmit={this.handleSubmit}>
+        <TextField
+          name="title"
+          label="Movie title"
+          placeholder="Input new movie title"
+          value={title}
+          onChange={this.handleChange}
+          error={titleError}
+        />
+
+        <TextField
+          name="description"
+          label="Movie description"
+          placeholder="Input new movie description"
+          value={description}
+          onChange={this.handleChange}
+        />
+
+        <TextField
+          name="imgUrl"
+          label="Movie imgUrl"
+          placeholder="Input new movie imgUrl"
+          value={imgUrl}
+          onChange={this.handleChange}
+        />
+
+        <TextField
+          name="imdbUrl"
+          label="Movie imdbUrl"
+          placeholder="Input new movie imdbUrl"
+          value={imdbUrl}
+          onChange={this.handleChange}
+        />
+
+        <TextField
+          name="imdbId"
+          label="Movie imdbId"
+          placeholder="Input new movie imdbId"
+          value={imdbId}
+          onChange={this.handleChange}
+        />
+
+        <button type="submit" className="button is-link">
+          Add movie
+        </button>
       </form>
     );
   }
 }
+
+NewMovie.propTypes = {
+  onAdd: PropTypes.func.isRequired,
+};
